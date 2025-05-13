6dced2438fd11f87e967f3989f7bb0934d10e254
Anamsaa <anmarsy07@gmail.com>
2025-05-13 12:36:29 +0200

    Estilizando los Registros de usuarios

---
+++ resources/views/formularios/registro-restaurantes.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.formulario.layout-formulario')
+@section('title', 'Crear perfil')
+@section('description', 'Registra tu restaurante en Foodies y atrae a nuevos clientes a tu negocio.')
+
+@section('contenido-formulario')
+
+    <div class="contenedor-registro-restaurante">
+        <div class="titulo-formulario-restaurante">
+            <h1>Creación de perfil - restaurantes</h1>
+        </div>
+        <form action="">
+            <div class="grid">
+                <div class="column">
+                    <div class="contenedor-formulario">
+                        <label for="nombre">Nombre del establecimiento </label>
+                        <input type="text" id="nombre" name="nombre" placeholder="Ej: Tacos XtremeFusion" required>
+                    </div>
+                    <div class="contenedor-formulario">
+                        <label for="horarios">Comenta brevemente los horarios que maneja tu negocio: </label>
+                        <textarea name="horarios" id="horarios" rows="6" cols="50" placeholder="Ej: Jueves y Viernes 18:00 a 22:00 | Domingos y Sábados de 18:00 a 23:00"></textarea>
+                    </div>
+                    <div class="contenedor-formulario">
+                        <p>Días de apertura:</p>
+                        <input type="checkbox" id="lunes" name="lunes" value="Lunes">
+                        <label for="lunes">Lunes</label><br>
+                        <input type="checkbox" id="martes" name="martes" value="Martes">
+                        <label for="martes">Martes</label><br>
+                        <input type="checkbox" id="miercoles" name="miercoles" value="Miércoles">
+                        <label for="miercoles">Miércoles</label>
+                        <input type="checkbox" id="jueves" name="jueves" value="Jueves">
+                        <label for="jueves">Jueves</label><br>
+                        <input type="checkbox" id="viernes" name="viernes" value="Viernes">
+                        <label for="viernes">Viernes</label><br>
+                        <input type="checkbox" id="sabado" name="sabado" value="Sábado">
+                        <label for="sabado">Sábado</label>
+                        <input type="checkbox" id="domingo" name="domingo" value="Domingo">
+                        <label for="domingo">Domingo</label>
+                    </div>
+                    <div class="contenedor-formulario">
+                        <label for="link-restaurante">Link de tu web oficial: </label>
+                        <input type="text" id="link-restaurante" name="link-restaurante" placeholder="Ej: www.mirestaurante.es">
+                    </div>
+                    <div class="contenedor-formulario">
+                        <label for="tipo-restaurante">Elige el tipo de restaurante: </label>
+                        <select name="tipo" id="tipo">
+                            <option value=""></option>
+                        </select>
+                        <div class="other-restaurant"></div>
+                    </div>
+                </div>
+
+                <div class="column">
+                    <div class="contenedor-formulario">
+                        <label for="invitacion">Invita a los foodies a tu negocio: </label>
+                        <textarea name="invitacion" id="invitacion" rows="6" cols="50" placeholder="Ej: '¡Bienvenido a nuestro restaurante! Aquí nuestra especialidad son los tacos al Pastor"></textarea>
+                    </div>
+
+                    <div class="contenedor-imagenes">
+                        <div class="contenedor-formulario">
+                            <label for="imagen-perfil">Escoge una foto de perfil</label>
+                            <input type="file" name="imagen-perfil">
+                        </div>
+                        <div class="contenedor-formulario">
+                            <label for="imagen-portada">Escoge una foto de portada</label>
+                            <input type="file" name="imagen-portada">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="button-formulario">
+                <button class="button-restaurantes-formulario" type="submit">Siguiente</button>
+            </div>
+        </form>
+    </div>
+    
+@endsection
--- resources/views/formularios/registro-usuario.blade.php
+++ resources/views/formularios/registro-usuario.blade.php
@@ -1,5 +1,5 @@
-@extends('layouts.formulario.background-formulario')
-@section('title', 'Regístrate')
+@extends('layouts.formulario.layout-formulario')
+@section('title', 'Crear perfil')
 @section('description', 'Regístrate en Foodies donde puedes compartir tus platos favoritos y conectar con amantes de la gastronomía como tú.')
 
 @section('contenido-formulario')
@@ -9,60 +9,69 @@
             <h1>Creación de perfil</h1>
         </div>
         <form action="">
-            <div class="contenedor-formulario">
-                <label for="nombre">Nombres: </label>
-                <input type="text" id="nombre" name="nombre" placeholder="Julian" required>
-            </div>
-            <div class="contenedor-formulario">
-                <label for="apellidos">Apellidos: </label>
-                <input type="text" id="apellidos" name="apellidos" placeholder="Moreno" required>
-            </div>
-            <div class="contenedor-formulario">
-                <label for="fnacimiento">Fecha de nacimiento: </label>
-                <input type="date" id="fnacimiento" name="fnacimiento" required>
-            </div>
-            <div class="contenedor-formulario">
-                <label for="pais">País: </label>
-                <select name="pais" id="pais">
-                    <option value=""></option>
-                </select>
-            </div>
-            <div class="contenedor-formulario">
-                <label for="region">Región: </label>
-                <select name="region" id="region">
-                    <option value=""></option>
-                </select>
-            </div>
-            <div class="contenedor-formulario">
-                <label for="ciudad">Ciudad: </label>
-                <select name="region" id="region">
-                    <option value=""></option>
-                </select>
-            </div>
-            <div class="contenedor-formulario">
-                <label for="comida">Escribe las 3 comidas que más te gusten: </label>
-                <div class="comidas-favoritas">
-                    <input type="text" id="comida1" name="comida" required>
-                    <input type="text" id="comida2" name="comida" required>
-                    <input type="text" id="comida3" name="comida" required>
+            <div class="grid">
+                <div class="column">
+                    <div class="contenedor-formulario">
+                        <label for="nombre">Nombres: </label>
+                        <input type="text" id="nombre" name="nombre" placeholder="Ej: Julian" required>
+                    </div>
+                    <div class="contenedor-formulario">
+                        <label for="apellidos">Apellidos: </label>
+                        <input type="text" id="apellidos" name="apellidos" placeholder="Ej: Moreno" required>
+                    </div>
+                    <div class="contenedor-formulario">
+                        <label for="fnacimiento">Fecha de nacimiento: </label>
+                        <input type="date" id="fnacimiento" name="fnacimiento" required>
+                    </div>
+                    <div class="contenedor-formulario">
+                        <label for="pais">País: </label>
+                        <select name="pais" id="pais">
+                            <option value=""></option>
+                        </select>
+                    </div>
+                    <div class="contenedor-formulario">
+                        <label for="region">Región: </label>
+                        <select name="region" id="region">
+                            <option value=""></option>
+                        </select>
+                    </div>
+                    <div class="contenedor-formulario">
+                        <label for="ciudad">Ciudad: </label>
+                        <select name="region" id="region">
+                            <option value=""></option>
+                        </select>
+                    </div>
                 </div>
-            </div>  
-            <div class="contenedor-formulario">
-                <label for="descripcion-usuario">Da una breve descripción de ti</label>
-                <textarea name="descripcion-usuario" id="descripcion-usuario" rows="6" cols="50"></textarea>
-            </div> 
 
-            <div class="contenedor-imagenes">
-                <div class="contenedor-formulario">
-                    <label for="imagen-perfil">Escoge una foto de perfil</label>
-                    <input type="file" name="imagen-perfil">
-                </div>
-                <div class="contenedor-formulario">
-                    <label for="imagen-portada">Escoge una foto de portada</label>
-                    <input type="file" name="imagen-portada">
+                <div class="column">
+                    <div class="contenedor-formulario">
+                        <label for="comida">Escribe las 3 comidas que más te gusten: </label>
+                        <div class="comidas-favoritas">
+                            <input type="text" id="comida1" name="comida" required>
+                            <input type="text" id="comida2" name="comida" required>
+                            <input type="text" id="comida3" name="comida" required>
+                        </div>
+                    </div>  
+                    <div class="contenedor-formulario">
+                        <label for="descripcion-usuario">Da una breve descripción de ti</label>
+                        <textarea name="descripcion-usuario" id="descripcion-usuario" rows="6" cols="50"></textarea>
+                    </div>  
+
+                    <div class="contenedor-imagenes">
+                        <div class="contenedor-formulario">
+                            <label for="imagen-perfil">Escoge una foto de perfil</label>
+                            <input type="file" name="imagen-perfil">
+                        </div>
+                        <div class="contenedor-formulario">
+                            <label for="imagen-portada">Escoge una foto de portada</label>
+                            <input type="file" name="imagen-portada">
+                        </div>
+                    </div>
                 </div>
             </div>
-            
+            <div class="button-formulario">
+                <button class="button-usuarios-formulario" type="submit">Cargar perfil</button>
+            </div>
         </form>
     </div>
     
