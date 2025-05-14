b75f3ca07cedc8e977c148ad64623be9065b3216
Anamsaa <anmarsy07@gmail.com>
2025-05-15 00:47:38 +0200

    Creación del primer componente

---
+++ resources/views/components/foodies/review-user.blade.php
@@ -0,0 +1,40 @@
+
+{{-- Componente para reviews --}}
+<div class="review">
+    <div class="review-header">
+        <img src="{{ asset($profilePic) }}" alt="Foto de {{ $username }}">
+        <div class="review-info">
+            <h3>{{ $username }}</h3>
+            <p>{{ $timestamp }}</p>
+            <p>{{ $restaurantName }}, {{ $location }}</p>
+        </div>
+    </div>
+    <div class="review-content">
+        <img src="{{ asset($image) }}" alt="{{ $restaurantName }}">
+        <div class="review-rating">
+            <h4>{{ $rating }}/10</h4>
+        </div>
+        <p>{{ $reviewText }}</p>
+    </div>
+    <div class="review-actions">
+        <button>{{ $likes }} Me gusta</button>
+        <button>{{ $comments }} Comentarios</button>
+    </div>
+</div>
+
+{{-- Ejemplo de como se usa @foreach dentro de una vista para pasar datos desde el controlador--}}
+
+@foreach($posts as $post)
+    <x-post 
+        :profilePic="$post->user->profile_pic"
+        :username="$post->user->name"
+        :timestamp="$post->created_at->diffForHumans()"
+        :restaurantName="$post->restaurant->name"
+        :location="$post->restaurant->location"
+        :image="$post->image"
+        :rating="$post->rating"
+        :reviewText="$post->review_text"
+        :likes="$post->likes"
+        :comments="$post->comments"
+/>
+@endforeach
