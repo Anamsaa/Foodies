ec094c33c02977ea36bb944f1a22dbd77bebe49d
Anamsaa <anmarsy07@gmail.com>
2025-05-22 14:13:44 +0200

    Adaptación de medidas de la imagen de perfil

--- resources/views/personas/perfil.blade.php
+++ resources/views/personas/perfil.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.layout-menu-panel')
+@extends('layouts.layout-perfil')
 @section('title', 'Ajustes')
 @section('description', 'Configura tu perfil y conecta con otras personas.')
 @section('content')
